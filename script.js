9ae70c8cb517f06d9264946a1e5737a82e14ac6b
Julien RICHARD <julien.y.richard@gmail.com>
2023-10-03 10:33:09 +0200

    Initialisation du projet Demo Node, Npm

---
+++ script.js
@@ -0,0 +1,7 @@
+//import sum from './lesson.js';
+import chalk from "chalk";
+import printMsg from '@monishcm/simple-hello-world-example2';
+
+//console.log(sum(2, 15));
+console.log(chalk.blue("Hello"))
+console.log(printMsg())
